2bbd5cdb3ba372eeacb0c13876046419908a6063
younjaeyeal <jyyoun1021@gmail.com>
2022-08-01 22:46:07 +0900

    공백을 포함한 수의 대소문자변환

---
+++ src/main/java/codej/programmers/demo/level1/StrangeString.java
@@ -0,0 +1,26 @@
+package codej.programmers.demo.level1;
+
+import java.util.Scanner;
+
+public class StrangeString {
+
+    public static String solution(String s){
+        String answer ="";
+        int count =0;
+        String[] list = s.split("");
+
+        for (String str : list) {
+            count = str.contains(" ")? 0 : count+1;
+            answer += (count %2 == 0) ? str.toUpperCase() : str.toLowerCase();
+
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(StrangeString.solution(s));
+    }
+}
