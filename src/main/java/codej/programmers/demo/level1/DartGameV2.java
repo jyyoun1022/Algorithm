4b5b567d741406885bfbfe32ad882a686f4e9286
younjaeyeal <jyyoun1021@gmail.com>
2022-08-31 00:01:43 +0900

    다트 V2

---
+++ src/main/java/codej/programmers/demo/level1/DartGameV2.java
@@ -0,0 +1,55 @@
+package codej.programmers.demo.level1;
+
+import java.util.Arrays;
+
+public class DartGameV2 {
+    public static void main(String[] args) {
+        String dartResult = "1S2D*3T";
+        int answer = 0;
+        int[] arr = new int[3];
+        int idx = 0;
+
+        char[] charArr = dartResult.toCharArray();
+        for (int i = 0; i < charArr.length; i++) {
+            int num = Character.getNumericValue(charArr[i]);
+            if (charArr[i] >= '0' && charArr[i] <= '9') {
+                if (charArr[i] == '1' && charArr[i + 1] == '0') {
+                    arr[idx] = 10;
+                    idx++;                                  //idx=1
+                }
+                arr[idx] = num;
+               idx++;
+            } else {
+                switch (charArr[i]) {
+                    case 'S':
+                        arr[idx - 1] = (int) (Math.pow(arr[idx - 1], 1));
+                        break;
+                    case 'D':
+                        arr[idx - 1] = (int) (Math.pow(arr[idx - 1], 2));
+                        break;
+                    case 'T':
+                        arr[idx-1] = (int) (Math.pow(arr[idx - 1], 3));
+                        break;
+                    case '*':
+                        if (idx - 2 >= 0) {
+                            arr[idx - 2] *= 2;
+                        }
+                        arr[idx -1] *= 2;
+                        break;
+                    case '#':
+                        arr[idx - 1] *= -1;
+                        break;
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+
+
+
+
+        }
+        for (int i1 : arr) {
+            answer += i1;
+        }
+        System.out.println(answer);
+    }
+}
